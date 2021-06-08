afec2c42e51c6b27c6b0c6f92c7f4459a9339705
Mathieu Witcz <mathieu.witcz@edifixio.com>
2021-06-08 12:10:04 +0200

    update vscode bootstrap script

--- bootstrap-vscode.sh
+++ bootstrap-vscode.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 chmod +x ~/dotfiles/bootstrap.sh
-~/dotfiles/bootstrap.sh --no-zsh --no-homebrew --no-nerd-fonts --no-terminator --no-git-configuration
+~/dotfiles/bootstrap.sh --no-zsh --no-homebrew --no-nerd-fonts --no-terminator --no-git-configuration --no-node
 exec zsh
