acd1f61d186f984765cbe418a6bcf72de9c283ca
Mathieu Witcz <mathieu.witcz@edifixio.com>
2021-06-08 12:15:25 +0200

    fix arg for bootstrap-vscode

--- bootstrap-vscode.sh
+++ bootstrap-vscode.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 chmod +x ~/dotfiles/bootstrap.sh
-~/dotfiles/bootstrap.sh --no-zsh --no-homebrew --no-nerd-fonts --no-terminator --no-git-configuration --no-node
+~/dotfiles/bootstrap.sh --no-zsh --no-homebrew --no-nerd-fonts --no-terminator --no-git-config --no-node
 exec zsh
